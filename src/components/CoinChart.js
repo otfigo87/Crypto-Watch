bd4ce40fcff46bd1048c187a76859242c889ec46
Otmane Aatik <aaotmane@gmail.com>
2023-03-31 15:29:01 -0400

    charts color gradient

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -73,6 +73,12 @@ const CoinChart = ({ coinId, coinName }) => {
         data={coinData}
         margin={{ top: 10, right: 0, left: 100, bottom: 0 }}
       >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="7%" stopColor={colors.color1} stopOpacity={0.8} />
+            <stop offset="93%" stopColor={colors.white1} stopOpacity={0} />
+          </linearGradient>
+        </defs>
         <XAxis dataKey="date" />
         <YAxis domain={["auto", "auto"]} />
         <CartesianGrid strokeDasharray="3 3" />
@@ -82,7 +88,7 @@ const CoinChart = ({ coinId, coinName }) => {
           dataKey="price"
           strokes={colors.color1}
           fillOpacity={1}
-          full="url(#colorUv)"
+          fill="url(#colorUv)"
         />
       </AreaChart>
     </div>
