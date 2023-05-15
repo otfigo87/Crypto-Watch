7f0c946324551f4971aa456bde853281554a21a1
Otmane Aatik <aaotmane@gmail.com>
2023-05-15 13:17:56 -0400

    reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
+import stableReducer from "./stable.reducer";
 
 export default combineReducers({
     //Reducers
-    
+    stableReducer
 })
---
+++ src/reducer/stable.reducer.js
@@ -0,0 +1,5 @@
+const initialState = { showStable : true };
+
+export default function stableReducer (state=initialState, action){
+    return state;
+}
