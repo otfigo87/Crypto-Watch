95a2dbee5ba0beef1d573196fc1212822de8cf59
Otmane Aatik <aaotmane@gmail.com>
2023-05-15 15:46:43 -0400

    setShowFavList action

--- src/components/TableFilters.js
+++ src/components/TableFilters.js
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setStableState } from "../action/stable.action";
+import { setListDisplay } from "../action/list.action";
 
 const TableFilters = () => {
 
   const [showStable, setShowStable] = useState(true);
+  const [showFavList, setShowFavList] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setStableState(showStable))
-  },[showStable]);
+    dispatch(setStableState(showStable));
+    dispatch(setListDisplay(showFavList));
+  },[showStable, showFavList]);
 
   return (
     <div className="table-filters">
       <div className="table-filters-container">
         <div className="stable-checkbox-container">
-          <input type="checkbox" id="stableCoin" defaultChecked={true}
-          onChange={() => setShowStable(!showStable)} />
-          <label htmlFor="stableCoin" >
-             {showStable ? "With Stable Coins" : "No Stable Coins"}   
+          <input
+            type="checkbox"
+            id="stableCoin"
+            defaultChecked={true}
+            onChange={() => setShowStable(!showStable)}
+          />
+          <label htmlFor="stableCoin">
+            {showStable ? "With Stable Coins" : "No Stable Coins"}
           </label>
         </div>
-        <div className="no-list-btn">
+        <div className="no-list-btn" onClick={() => setShowFavList(false)}>
           <p>No List</p>
         </div>
-        <div className="fav-list">
+        <div className="fav-list" onClick={() => setShowFavList(true)}>
           <p>Favorites</p>
           <img src="./assets/star-full.svg" alt="icon star" />
         </div>
