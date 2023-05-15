9a9d452b1ea7f37b886907bc39a5251651340ba3
Otmane Aatik <aaotmane@gmail.com>
2023-05-15 13:09:17 -0400

    redux setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './styles/index.scss'
+import './styles/index.scss';
+
+//Redux
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: rootReducer
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
+  <Provider store={store}>
+      <App />
+  </Provider>
  
 );
 
