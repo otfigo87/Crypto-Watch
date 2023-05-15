3aa74464c2981a4f97a2d4c5509622ee4e565933
Otmane Aatik <aaotmane@gmail.com>
2023-05-15 13:34:40 -0400

    dispatch stable action

--- src/components/TableFilters.js
+++ src/components/TableFilters.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setStableState } from "../action/stable.action";
 
 const TableFilters = () => {
+
+  const [showStable, setShowStable] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setStableState(showStable))
+  },[showStable]);
+
   return (
     <div className="table-filters">
       <div className="table-filters-container">
         <div className="stable-checkbox-container">
-          <input type="checkbox" id="stableCoin" defaultChecked={true} />
+          <input type="checkbox" id="stableCoin" defaultChecked={true}
+          onChange={() => setShowStable(!showStable)} />
           <label htmlFor="stableCoin" >
             With Stable Coins
           </label>
